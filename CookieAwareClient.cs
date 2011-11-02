80caeca51f1e0e37baf19afe43da59d55495c1ea
Andre Brutus <anebg.net@gmail.com>
2011-11-02 11:53:49 -0400

    Added Cookie Class (With Proper Credit)

---
+++ CookieAwareClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace MCrawl
+{
+    /**
+     * "Credit Where Credit is Due"
+     * Cookie Aware web client courtesy of "lasseespeholt"
+     * http://stackoverflow.com/questions/5869785/post-username-and-password-to-login-page-programmatically/5869858#5869858
+     */
+    internal class CookieAwareWebClient : WebClient
+    {
+        private CookieContainer cookieContainer = new CookieContainer();
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest)
+            {
+                (request as HttpWebRequest).CookieContainer = cookieContainer;
+            }
+            return request;
+        }
+    }
+}
